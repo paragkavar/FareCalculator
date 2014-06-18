6f5a3c26b66b6b0f431c1e65d3c538863e8d4fc4
Subash Luitel <luitel@me.com>
2014-06-17 22:04:55 -0500

    initial commit

---
+++ FareCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FareCalculator
+//
+//  Created by Subash Luitel on 6/17/14.
+//  Copyright (c) 2014 Luitel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FareCalculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FareCalculator
+//
+//  Created by Subash Luitel on 6/17/14.
+//  Copyright (c) 2014 Luitel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
